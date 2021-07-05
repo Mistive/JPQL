3fad5637c48c6bb90510ab65919fe8b0cdeb05c9
mistive <pms9725@gmail.com>
2021-07-06 00:16:31 +0900

    JPQL 기본 & 사용자정의 함수

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -18,20 +18,22 @@ public class jpaMain {
         tx.begin();
         try {
 
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
             Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
+            member.setUsername("관리자1");
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
-            String query= "select m from Member m, Team t where m.username = t.name";
-            List<Member> result = em.createQuery(query, Member.class)
+            String query = "select group_concat(m.username) from Member m";
+
+            List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
+
+            for (String s : resultList) {
+                System.out.println("s = " + s);
+            }
 
             tx.commit();
         } catch (Exception e) {
