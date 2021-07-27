2de845bc206f58e7f78d245f5b6ce6e7d5d22883
mistive <pms9725@gmail.com>
2021-07-27 15:29:40 +0900

    경로표현식

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 
 public class jpaMain {
@@ -18,21 +19,33 @@ public class jpaMain {
         tx.begin();
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
             Member member = new Member();
             member.setUsername("관리자1");
+            member.setTeam(team);
             em.persist(member);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
-            String query = "select group_concat(m.username) from Member m";
+            em.flush();
+            em.clear();
+
+           String query = "select t.name from Member m join m.team t";
 
             List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (String s : resultList) {
-                System.out.println("s = " + s);
+            for (String team1 : resultList) {
+                System.out.println("team1 = " + team1);
             }
 
             tx.commit();
